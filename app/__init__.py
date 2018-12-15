478689ce96435fb4a5a20f4732ed6e796f41e172
Munira <munniomer@gmail.com>
2018-12-15 21:17:36 +0300

    registers the blueprint of V1

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,8 @@ def create_app(config_name):
     app = Flask(__name__)
     app.url_map.strict_slashes = False
     app.config.from_object(app_config[config_name])
+
+    from app.api.v1 import v1
+    app.register_blueprint(v1)
+
     return app
-
-
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
